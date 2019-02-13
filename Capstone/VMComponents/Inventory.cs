22b2bc33c8d432d2ff5628b12eaac76d5a7114f6
forrest rojas <forrest.rojas@gmail.com>
2019-02-13 18:22:47 -0500

    Removed try and catch from the inventory class NOW

--- Capstone/VMComponents/Inventory.cs
+++ Capstone/VMComponents/Inventory.cs
@@ -25,7 +25,6 @@ namespace Capstone.VMComponents
                     string currentLine = sr.ReadLine();
                     string[] itemInfo = currentLine.Split('|');
 
-
                     VendingMachineProduct vendingMachineProduct = Assembly.GetExecutingAssembly().CreateInstance("Capstone.VMComponents." + itemInfo[itemInfo.Length - 1], true, BindingFlags.Default, null, new string[] { itemInfo[1], itemInfo[2] }, null, null) as VendingMachineProduct;
 
                     if (vendingMachineProduct != null )
@@ -55,21 +54,14 @@ namespace Capstone.VMComponents
                     string currentLine = sr.ReadLine();
                     string[] itemInfo = currentLine.Split('|');
 
-                    try
-                    {
-                        VendingMachineProduct vendingMachineProduct = Assembly.GetExecutingAssembly().CreateInstance("Capstone.VMComponents." + itemInfo[itemInfo.Length - 1], true, BindingFlags.Default, null, new string[] { itemInfo[1], itemInfo[2] }, null, null) as VendingMachineProduct;
+                    VendingMachineProduct vendingMachineProduct = Assembly.GetExecutingAssembly().CreateInstance("Capstone.VMComponents." + itemInfo[itemInfo.Length - 1], true, BindingFlags.Default, null, new string[] { itemInfo[1], itemInfo[2] }, null, null) as VendingMachineProduct;
 
-                        if (vendingMachineProduct != null)
-                        {
-                            inventory.Add(itemInfo[0], vendingMachineProduct);
-                        }
-                    }
-                    catch (Exception ex)
+                    if (vendingMachineProduct != null)
                     {
-                        Console.Write("Inventory could not fully load.");
-                        Console.Write($"Line number: {i} could not be deserialized.");
-                        throw ex;
+                        inventory.Add(itemInfo[0], vendingMachineProduct);
                     }
+                    //Console.Write("Inventory could not fully load.");
+                    //Console.Write($"Line number: {i} could not be deserialized.");
                     i++;
                 }
             }
